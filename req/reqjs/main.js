89adf292fd2afd2a168b10a9d452854b5363370a
GoldRenard <sadmanyt@gmail.com>
2022-02-26 17:17:36 +0200

    22w26c

---
+++ req/reqjs/main.js
@@ -0,0 +1,15 @@
+$("bug").on("click", function () {
+  window.location.href = "/bug/";
+});
+
+$("suggest").on("click", function () {
+  window.location.href = "/suggest/";
+});
+
+$("help").on("click", function () {
+  window.location.href = "/help/";
+});
+
+$("traa").on("click", function () {
+  window.location.href = "/help/index.html";
+});
