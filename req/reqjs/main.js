533a55b5af07a2bf11307008ee47657b49d27bbc
Alexey Natalushko <91388240+GoldRetard@users.noreply.github.com>
2022-03-01 12:28:18 +0200

    Update main.js

--- req/reqjs/main.js
+++ req/reqjs/main.js
@@ -16,26 +16,9 @@ if (title = "") {
   } else if (message == "") {
     $("#request_anonymous_requester_email_error").text("Инициатор: Сообщение: поле не может быть пустым");
     return false;
-  }
+  };
 
   $("#request_anonymous_requester_email_error").text("");
-
-  /*
-  $.ajax({
-    url: 'php/main.php',
-    type: 'POST',
-    cache: false,
-    data: { 'title': title 'name': name, 'email': email, 'message': message },
-    dataType: 'html',
-    beforeSend: function() {
-      $("#button").prop("disabled", true);
-    },
-    success: function(data) {
-      alert(data);
-      $("#button").prop("disabled", false);
-    }
-  })
-  */
 }),
   
 console.log("wooooork");
